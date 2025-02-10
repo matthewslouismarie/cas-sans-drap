27ebbfe8b7db44cbb5cd2f4c7f7c476f9dc29189
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2025-02-11 00:11:08 +0100

    Merge readme and notebook together and add queries to it

--- src/init.sh
+++ src/init.sh
@@ -6,3 +6,4 @@ apt install python3.10-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip install pandas
+apt install -y texlive-xetex texlive-fonts-recommended texlive-plain-generic pandoc
