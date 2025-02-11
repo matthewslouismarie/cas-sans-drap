7bff02468afb73a89ccbdc07dba6abf908b7e332
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2025-02-12 00:18:10 +0100

    Final commit

--- src/init.sh
+++ src/init.sh
@@ -6,4 +6,13 @@ apt install python3.10-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip install pandas
+
+# To be able to export the notebook to PDF
 apt install -y texlive-xetex texlive-fonts-recommended texlive-plain-generic pandoc
+
+# Download and install DSBulk
+wget https://github.com/datastax/dsbulk/releases/download/1.11.0/dsbulk-1.11.0.tar.gz
+tar -zxvf dsbulk-1.11.0.tar.gz
+mv dsbulk-1.11.0/ /opt/
+echo 'PATH=/opt/dsbulk-1.11.0/bin/:$PATH' >> .profile
+source .profile
